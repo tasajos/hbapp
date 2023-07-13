964701fa2f32871444afea5b4d2a5da279cc89cf
tasajos <tasajos@gmail.com>
2023-07-13 09:51:43 -0400

    update logins

---
+++ app/src/main/java/org/chakuy/hbsis/insertar_registro.java
@@ -0,0 +1,14 @@
+package org.chakuy.hbsis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insertar_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_registro);
+    }
+}
